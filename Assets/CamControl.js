704925becc7d814e334bfb80f32ae13f8b53bc60
Alex Morais <thatsamorais@mail.com>
2012-11-17 13:31:44 -0700

    Added the Google App Engine directory, and edited several game scripts. Still working on some parts, but this is stable.

--- Assets/CamControl.js
+++ Assets/CamControl.js
@@ -21,14 +21,27 @@ function Start () {
 function Update () {
 
 	camPos = gameObject.Find("Main Camera").transform.position;
-	if(Input.GetKey(KeyCode.LeftControl))
+	if(Input.GetKey(KeyCode.LeftShift))
 	{
+		/* Enable camera movement */
+	
+    	gameObject.Find("Main Camera").transform.position = Vector3(Mathf.Clamp(camPos.x + Input.GetAxis("Mouse X"), posMin, posMax),
+																	Mathf.Clamp(camPos.y + Input.GetAxis("Mouse ScrollWheel"), htMin, htMax),
+																	Mathf.Clamp(camPos.z + Input.GetAxis("Mouse Y"), posMin, posMax));
+		
+		gameObject.Find("Point light").light.intensity = gameObject.Find("Main Camera").transform.position.y*0.2;
+	}
+	else
+	{
+		/* Mouse Picking */
+	
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hit : RaycastHit;
+		var layerMask : LayerMask = -1;
+		
 		if(Input.GetButtonDown("Fire1"))
-		{
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			var hit : RaycastHit;
-			var layerMask : LayerMask = -1;
-			
+		{			
+			Debug.Log("Fire1 pressed");
 			layerMask = 1 << LayerMask.NameToLayer("Forkers") |
 						1 << LayerMask.NameToLayer("Branchers") |
 						1 << LayerMask.NameToLayer("Buildings");
@@ -36,6 +49,7 @@ function Update () {
 			// Check if we hit a selectable object like a unit or building
 			if(Physics.Raycast(ray, hit, Mathf.Infinity, layerMask))
 			{
+				Debug.Log(String.Format("Targeted {0}", hit.transform.name));
 				if(null != currentSelection)
 				{
 					currentSelection.renderer.material.color = originalColorOfSelected;
@@ -46,18 +60,25 @@ function Update () {
 				currentSelection.renderer.material.color = Color.magenta;
 			}
 		}
-		
-		if((null != currentSelection) && (Input.GetButtonDown("Fire2")))
+		else if((null != currentSelection) && (Input.GetButtonDown("Fire2")))
 		{
+			Debug.Log("Fire2 pressed");
+			layerMask = 1 << LayerMask.NameToLayer("Selectable") | 
+						1 << LayerMask.NameToLayer("Trees");
 			// Operate on targetedSelection accordingly
+			if(Physics.Raycast(ray, hit, Mathf.Infinity, layerMask))
+			{
+				Debug.Log(String.Format("Targeted {0}", hit.transform.name));
+				if(null != targetedSelection)
+				{
+					targetedSelection.renderer.material.color = originalColorOfTargeted;
+				}
+
+				Debug.Log(String.Format("hit ({0})", hit.transform.name));
+				targetedSelection = hit.transform;
+				originalColorOfTargeted = targetedSelection.renderer.material.color;
+				targetedSelection.renderer.material.color = Color.green;
+			}
 		}
-	}	
-	else
-    {
-    	gameObject.Find("Main Camera").transform.position = Vector3(Mathf.Clamp(camPos.x + Input.GetAxis("Mouse X"), posMin, posMax),
-																	Mathf.Clamp(camPos.y + Input.GetAxis("Mouse ScrollWheel"), htMin, htMax),
-																	Mathf.Clamp(camPos.z + Input.GetAxis("Mouse Y"), posMin, posMax));
-		
-		gameObject.Find("Point light").light.intensity = gameObject.Find("Main Camera").transform.position.y*0.2;
 	}
 }
--- Assets/HexBoardScript.js
+++ Assets/HexBoardScript.js
@@ -4,23 +4,93 @@ var base : Transform;
 var tree : Transform;
 var forker : Transform;
 var brancher : Transform;
+var branch : Transform;
+var fork : Transform;
 var fortification : Transform;
-var number_of_trees : int;
-var number_of_forkers : int;
-var number_of_branchers : int;
+var init_number_of_trees : int;
+var init_number_of_forkers : int;
+var init_number_of_branchers : int;
 var y_placement : float;
-
 var groundTexture1 : Texture2D;
 var groundTexture2 : Texture2D;
-
-// "constants" (though not really, just in practice)
+// "constants"
 var lo_index : int = 0;
 var hi_index : int = 32;
+
+private var forkerCount : int;
+private var brancherCount : int;
+private var branchCount : int;
+private var forkCount : int;
+private var fortificationCount : int;
+
+private var brancher_positions : Array = new Array(new Vector2(27, 24),
+												   new Vector2(26, 25),
+												   new Vector2(25, 27),
+												   new Vector2(23, 28));
+private var forker_positions : Array = new Array(new Vector2(6,3),
+												 new Vector2(6,5),
+												 new Vector2(6,7),
+												 new Vector2(4,7));
+
+
+function SetupPiece(piece : Transform, x, y, name)
+{
+	piece.parent = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform;
+	piece.transform.position = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform.position;
+	piece.transform.position.y = y_placement;
+	piece.transform.localScale = Vector3(1, 1, 1);
+	piece.name = name;
+}
+
+function CreateBase(x, y, color:Color, name)
+{
+	var baseClone : Transform = Instantiate(base);
+	SetupPiece(baseClone, x, y, name);
+	baseClone.renderer.material.color = color;
+	piece.transform.localScale = Vector3(2, 20, 2);
+}
+
+function CreateBranch(x, y)
+{
+	var branchClone : Transform = Instantiate(branch);
+	SetupPiece(branchClone, x, y, String.Format("branch_{0}", branchCount));
+	branchCount++;
+}
+
+function CreateForker(x, y)
+{
+	var forkerClone : Transform = Instantiate(forker);
+	SetupPiece(forkerClone, x, y, String.Format("forker_{0}", forkerCount));
+	forkerCount++;
+}
+
+function CreateFork(x, y)
+{
+	var forkClone : Transform = Instantiate(fork);
+	SetupPiece(forkClone, x, y, String.Format("fork_{0}", forkCount));
+	forkCount++;
+}
+
+function CreateBrancher(x, y)
+{
+	var brancherClone : Transform = Instantiate(brancher);
+	SetupPiece(brancherClone, x, y, String.Format("brancher_{0}", brancherCount));
+	brancherCount++;
+}
+
+
+function CreateFortification(x, y)
+{
+	var fortificationClone : Transform = Instantiate(fortification);
+	SetupPiece(fortificationClone, x, y, String.Format("fortification_{0}", fortificationCount));
+	fortificationCount++;
+}
+
+
 
 function Start () {
 
 	var tex : Texture2D = null;
-	var basePlaced : boolean = false;
 	var treesPlaced : int = 0;
 
 	// This loop is pretty grotesque, but 1024 iterations might not be too bad...
@@ -42,7 +112,7 @@ function Start () {
 			gameObject.Find(String.Format("/HexPlain/_{0}_{1}_", x, y)).renderer.material.mainTexture = tex;
 			
 			// Place trees
-			if((treesPlaced < number_of_trees) && (2 == Random.Range(1,20)))
+			if((treesPlaced < init_number_of_trees) && (2 == Random.Range(1,20)))
 			{
 				/*var treeClone : Transform = Instantiate(tree,
 														gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform.position,
@@ -51,66 +121,46 @@ function Start () {
 				treeClone.parent = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform;
 				treeClone.transform.position = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform.position;
 				treeClone.transform.position.y = y_placement;
-				treeClone.transform.localScale = Vector3(0.5, 0.5, 0.5);
+				treeClone.transform.localScale = Vector3(0.4, 0.4, 0.4);
 				treesPlaced += 1;
 			}
 		}
 	}
 
 	//Create the base.
-	var baseClone1 : Transform = Instantiate(base);
-	baseClone1.transform.position = gameObject.Find("/HexPlain/_3_3_").transform.position;
-	baseClone1.transform.position.y = y_placement;
-	baseClone1.renderer.material.color = Color.red;
-	baseClone1.name = "ForkerBase";
+	CreateBase(3, 3, Color.red, "ForkerBase");
 	
 
-	//for(var f=0; f < number_of_forkers; f++)
+	//for(var f=0; f < init_number_of_forkers; f++)
 	//{
 	//}
 	CreateForker(6,3);
+	CreateBranch(7,3);
 	CreateForker(6,5);
+	CreateBranch(7,5);
 	CreateForker(6,7);
+	CreateBranch(7,7);
 	CreateForker(4,7);
+	CreateBranch(5,7);
 	
 	//Create the base.
-	var baseClone2 : Transform = Instantiate(base);
-	baseClone2.transform.position = gameObject.Find("/HexPlain/_28_28_").transform.position;
-	baseClone2.transform.position.y = y_placement;
-	baseClone2.renderer.material.color = Color.blue;
-	baseClone2.name = "BrancherBase";
+	CreateBase(28, 28, Color.blue, "BrancherBase");
 	
-	//for(var b=0; b < number_of_branchers; b++)
+	//for(var b=0; b < init_number_of_branchers; b++)
 	//{
 	//}
-	CreateBrancher(27,24);
-	CreateBrancher(26,25);
-	CreateBrancher(25,27);
-	CreateBrancher(23,28);
+	CreateBrancher(27, 24);
+	CreateBranch(27, 23);
+	CreateBrancher(26, 25);
+	CreateBranch(26, 24);
+	CreateBrancher(25, 27);
+	CreateBranch(25, 26);
+	CreateBrancher(23, 28);
+	CreateBranch(23, 27);
 }
 
 function Update () {
 	if (Input.GetButtonDown("Fire1")) {
 		
 	}
-}
-
-function CreateForker(x, y)
-{
-	var forkerClone : Transform;
-	forkerClone = Instantiate(forker);
-	forkerClone.parent = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform;
-	forkerClone.transform.localScale = Vector3(1, 1, 1);
-	forkerClone.transform.position = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform.position;
-	forkerClone.transform.position.y = y_placement;
-}
-
-function CreateBrancher(x, y)
-{
-	var brancherClone : Transform;
-	brancherClone = Instantiate(brancher);
-	brancherClone.parent = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform;
-	brancherClone.transform.localScale = Vector3(1, 1, 1);
-	brancherClone.transform.position = gameObject.Find(String.Format("HexPlain/_{0}_{1}_", x, y)).transform.position;
-	brancherClone.transform.position.y = y_placement;
 }
